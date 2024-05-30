b3f06f3a5bf38b6e08ca164908fe5826a19075fa
taenam kim <ktn2674@naver.com>
2024-05-30 14:37:26 +0900

    first commit

---
+++ src/main/java/org/example/backstudy/BackstudyApplication.java
@@ -0,0 +1,13 @@
+package org.example.backstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackstudyApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BackstudyApplication.class, args);
+  }
+
+}
