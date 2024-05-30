b01183023f6e512f86990d9352571cb0631226a3
taenam kim <ktn2674@naver.com>
2024-05-30 15:10:51 +0900

    UserEntity 생성

--- src/main/java/org/example/backstudy/Entity/UserEntity.java
+++ src/main/java/org/example/backstudy/Entity/UserEntity.java
@@ -1,6 +1,31 @@
 package org.example.backstudy.Entity;
 
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long user_id;
+
+  private String user_name;
+
+  private String password;
+
+  private String email;
+
+
 }
